aec520c62eb098fa5a8da6fe6ffbc7711893f7da
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-01-02 12:48:24 -0600

    Modal now is accessible and removable with appropriate buttons

--- modal.js
+++ modal.js
@@ -1,52 +1,17 @@
-// Get the modal
-var modals = document.querySelectorAll("#modal");
-
-// Get all project images 
-var projectImages = document.querySelectorAll("#project-image");
-
-// Get all project titles 
-var projectTitles = document.querySelectorAll("#project-title");
-
-// Get all project title text
-var projectTitlesText = document.querySelectorAll("#project-title h3");
-
-// Get the <span> element that closes the modal
-var closeButtons = document.querySelectorAll(".close");
+var modal = document.querySelector(".modal");
+var infoButton = document.querySelector(".fa-question-circle");
+var startButton = document.querySelector(".start-button");
 
 // When the user clicks on the image, open the modal
-projectImages.forEach(btn => btn.addEventListener('click', e => {
-    const index = e.target.dataset.image;
-
-    modals[index].style.display = "block";
-
+infoButton.addEventListener('click', e => {
+    modal.style.display = "block";
     document.querySelector("#main").style.filter = "blur(2px)";
-}))
-
-// When the user clicks on the title, open the modal
-projectTitles.forEach(title => title.addEventListener('click', e => {
-
-    const index = e.target.dataset.image;
-
-    modals[index].style.display = "block";
-
-    document.querySelector("#main").style.filter = "blur(2px)";
-}))
-
-// When the user clicks on the title, open the modal
-projectTitlesText.forEach(title => title.addEventListener('click', e => {
-
-    const index = e.target.dataset.image;
-
-    modals[index].style.display = "block";
-
-    document.querySelector("#main").style.filter = "blur(2px)";
-}))
+})
 
 // When the user clicks on <span> (x), close the modal
-closeButtons.forEach(button => button.addEventListener('click', e => {
-  const index = +e.target.dataset.span;
-  
-  modals[index].style.display = "none";
+startButton.addEventListener('click', e => {
+  console.log("test")
+  modal.style.display = "none";
   document.querySelector("#main").style.filter = "blur(0px)";
-}))
+});
 
