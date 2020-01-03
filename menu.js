150af8d0a8bdff676e6e495973c9626bde898f97
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-01-03 12:28:26 -0600

    Adding the option to click outside of the timer controls menu during mobile view, to close out the timer controls menu, instead of only having to click the hamburger menu.

--- menu.js
+++ menu.js
@@ -1,6 +1,13 @@
 const menu = document.querySelector(".menu");
-const timerControls = document.querySelector(".timerControls")
+const timerControls = document.querySelector(".timerControls");
+const timerContainer = document.querySelector(".displayTimeContainer");
 
 menu.addEventListener('click', (e) => {
     timerControls.classList.toggle('hidden');
+})
+
+timerContainer.addEventListener('click', e => {
+    if (window.innerWidth <= 600) {
+        timerControls.classList.toggle('hidden');
+    }
 })
