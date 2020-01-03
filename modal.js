cf26849a1115114009c37b58937d3ac1cedf232b
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-01-03 11:09:51 -0600

    Added a nonfunctioning hamburger menu

--- modal.js
+++ modal.js
@@ -1,14 +1,18 @@
 var modal = document.querySelector(".modal");
 var infoButton = document.querySelector(".fa-question-circle");
+var helpButton = document.querySelector(".help-button");
 var startButton = document.querySelector(".start-button");
 
-// When the user clicks on the image, open the modal
 infoButton.addEventListener('click', e => {
     modal.style.display = "block";
     document.querySelector("#main").style.filter = "blur(2px)";
 })
 
-// When the user clicks on <span> (x), close the modal
+helpButton.addEventListener('click', e => {
+    modal.style.display = "block";
+    document.querySelector("#main").style.filter = "blur(2px)";
+})
+
 startButton.addEventListener('click', e => {
   console.log("test")
   modal.style.display = "none";
