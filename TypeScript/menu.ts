9e9e906d188e368d40fd5d27a4cf6a570f065271
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-03-19 18:52:16 -0500

    Transitioned all files to .ts files. JavaScript functionaltiy is currently broken in app.

---
+++ TypeScript/menu.ts
@@ -0,0 +1,23 @@
+export const menu = document.querySelector(".menu") as HTMLDivElement;
+export const timerContainer = document.querySelector(".displayTimeContainer") as HTMLDivElement;
+import { timerControls } from "./index";
+
+// Adds clickability to the hamburger menu
+menu.addEventListener('click', (e) => {
+    timerControls.classList.toggle('hidden');
+})
+
+// If the mobile view is displayed and you click outside of the timer controls, then hide them (the menu)
+timerContainer.addEventListener('click', e => {
+    if (window.innerWidth <= 600 && !timerControls.classList.contains("hidden") && (e.target as HTMLButtonElement).dataset.method !== 'stop') {
+        timerControls.classList.add('hidden');
+    }
+})
+
+// On page load, if the view is mobile, hide the hamburger menu, and the timer controls so only the modal is displayed
+window.addEventListener('load', e => {
+    if (window.innerWidth <= 600) {
+        timerControls.classList.add("hidden");
+        menu.classList.add("hidden");
+    }
+});
